721b490f27fb904528cc730772827b9cdef35cc5
niquole <nicole.antonopoulou@gmail.com>
2019-03-27 12:11:39 +0100

    js

--- js/main.js
+++ js/main.js
@@ -2,25 +2,46 @@
 var req = new XMLHttpRequest();
 
 
-req.onclick = function() {
+req.onload = function() {
   var res = JSON.parse(req.responseText);
   
-  var greek = res.products.coffee;
+  var greek = res.products.coffee.greek;
   
-  for (let i=0; i < coffee.length; i++) {
+  for (let i=0; i < greek.length; i++) {
+    var container = document.createElement('li');
+    var containerText = document.createTextNode('');
     var ele = document.createElement('h2');
     var text = document.createTextNode(greek[i].name);
     ele.appendChild(text);
     
-    ele.classList.add('red');
+    var greekBlend = document.createElement('p');
+    var greekBlendText = document.createTextNode(greek[i].blend);
+    greekBlend.appendChild(greekBlendText);
+
+
+    var greekDesc = document.createElement('p');
+    var greekDescText = document.createTextNode(greek[i].description);
+    greekDesc.appendChild(greekDescText);
     
-    
-    document.querySelector('div').appendChild(ele);
+    var greekGrainGrinding = document.createElement('p');
+    var greekGrainGrindingText = document.createTextNode(greek[i].grainGrinding[0]);
+    greekGrainGrinding.appendChild(greekGrainGrindingText);
+
+    var greekPrice = document.createElement('p');
+    var GreekPriceText = document.createTextNode(greek[i].price);
+    greekPrice.appendChild(GreekPriceText);
+
+    container.appendChild(ele);
+    container.appendChild(greekDesc);
+    container.appendChild(greekBlend);
+    container.appendChild(greekGrainGrinding);
+    container.appendChild(greekPrice);
+    document.querySelector('.greek').appendChild(container);
   }
 }
 
 
-req.open("GET", "https://api.myjson.com/bins/myupq");
+req.open("GET", "https://api.myjson.com/bins/1ep0ie");
 
 req.send();
 
@@ -110,19 +131,19 @@ req.send();
 // }
 
 
-var currentImg = 0;
+// var currentImg = 0;
 
-document.querySelector('.overlay img').onclick = function() {
-  document.querySelector('.overlay').style.display = "none";
-}
+// document.querySelector('.overlay img').onclick = function() {
+//   document.querySelector('.overlay').style.display = "none";
+// }
 
-var allImg = document.querySelectorAll('.gallery .item img');
+// var allImg = document.querySelectorAll('.gallery .item img');
 
-for (let i = 0; i < allImg.length; i++) {
-  allImg[i].onclick = function() {
-    var clickedImg = this.src;
-    document.querySelector('.overlay img').src = clickedImg;
-    document.querySelector('.overlay').style.display = "flex";
-    currentImg = i;
-  }
-}
+// for (let i = 0; i < allImg.length; i++) {
+//   allImg[i].onclick = function() {
+//     var clickedImg = this.src;
+//     document.querySelector('.overlay img').src = clickedImg;
+//     document.querySelector('.overlay').style.display = "flex";
+//     currentImg = i;
+//   }
+// }
